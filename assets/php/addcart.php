57ef1cfca34297aa83e1e7ed4c8eafb06fdafba4
somsomdah <somdah98@gmail.com>
2021-11-21 00:33:07 +0900

    fix bug

--- assets/php/addcart.php
+++ assets/php/addcart.php
@@ -5,6 +5,9 @@ include "../db/connect.php";
 $count = $_POST["count"];
 $product_id = $_POST["product_id"];
 $user_id = $_SESSION['id'];
+if (!$user_id){
+    echo "<script> alert('Please login first.'); location.href = '".$_SERVER['HTTP_REFERER']."' </script>";
+}
 
 $query = sprintf("insert into Cart (user_id, product_id, count) value(%d,%d,%d)",$user_id, $product_id, $count);
 $res = mysqli_query($dbconn, $query);
