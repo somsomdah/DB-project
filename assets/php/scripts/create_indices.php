81675bcd6a13fc72370b59765f55b16b26bfb163
somsomdah <somdah98@gmail.com>
2021-11-21 12:19:05 +0900

    crate create_indices.php and modify db connection code

---
+++ assets/php/scripts/create_indices.php
@@ -0,0 +1,20 @@
+<?php
+
+include "../db/connect.php";
+
+if (mysqli_connect_errno()){
+    printf("Connection failed: %s <br/>", mysqli_connect_error());
+}
+else{
+    $query = "create index PurchaseIndex on Purchase(product_id, user_id)";
+
+    $res = mysqli_query($dbconn, $query);
+    if ($res===TRUE){
+        printf("index created <br/>");
+    }else{
+        printf("error creating index <br/>");
+    }
+
+    mysqli_close($dbconn);
+
+}
